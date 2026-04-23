206e72216bc74a430cd3c52c23c979df92c9221b
neha060705 <missneha6thjuly@gmail.com>
2026-04-23 11:54:03 +0530

    Update API URL for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://lostandfound-1-zk9m.onrender.com/api',
 });
 
 // Attach JWT token to every request
